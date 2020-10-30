e8f7d2b44cd1d75b478b7b34b20918960ebe1ca1
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-10-30 14:00:27 -0700

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,36 +1,27 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  your_hash = {"myhash" => "hash"}
 end
-
-
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
+ # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+def shipping_manifest
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
 end
+# Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+  }
+  shipping_manifest["oil paintings"] 
 end
-
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
+ # add 2 muskets to the shipping_manifest hash below
 
 
 
@@ -39,5 +30,15 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+  shipping_manifest["muskets" => 2]
+  shipping_manifest["muskets"]= 2
+  shipping_manifest["gun powder" => 4]
+  shipping_manifest["gun powder"]= 4
+  return shipping_manifest
 end
